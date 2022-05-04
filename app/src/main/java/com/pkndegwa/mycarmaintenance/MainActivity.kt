15473e2f0c2a41fb7023a6c9caa7a721e57faca9
Peter Kingori <kpndegwa@gmail.com>
2022-05-04 10:19:38 +0300

    Add the vehicle registration activity and an intent in the main activity to navigate to it.

--- app/src/main/java/com/pkndegwa/mycarmaintenance/MainActivity.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.pkndegwa.mycarmaintenance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pkndegwa.mycarmaintenance.databinding.ActivityMainBinding
 
+/**
+ * This activity is for the welcome page.
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Set up view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Launch the VehicleRegistrationActivity on addVehicleButton click
+        binding.addVehicleButton.setOnClickListener { vehicleRegistration() }
+    }
+
+    private fun vehicleRegistration() {
+        val intent = Intent(this, VehicleRegistrationActivity::class.java)
+        startActivity(intent)
     }
 }
