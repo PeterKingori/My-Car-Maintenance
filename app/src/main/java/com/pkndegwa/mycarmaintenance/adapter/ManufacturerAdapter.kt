e1417d71832483b75529219360cd36d7e987bdbe
Peter Kingori <kpndegwa@gmail.com>
2022-06-20 16:48:59 +0300

    Refactor the ManufacturerAdapter.kt class to use view binding. The label of the manufacturers fragment is also updated in the nav_graph.xml

--- app/src/main/java/com/pkndegwa/mycarmaintenance/adapter/ManufacturerAdapter.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/adapter/ManufacturerAdapter.kt
@@ -2,13 +2,10 @@ package com.pkndegwa.mycarmaintenance.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.pkndegwa.mycarmaintenance.R
+import com.pkndegwa.mycarmaintenance.databinding.ManufacturersListItemBinding
 import com.pkndegwa.mycarmaintenance.model.Manufacturer
 import com.pkndegwa.mycarmaintenance.ui.VehicleManufacturersFragmentDirections
 
@@ -21,18 +18,18 @@ class ManufacturerAdapter(private val context: Context, private val dataset: Lis
     /**
      * Provides a reference for the views needed to display items in the list.
      */
-    class ManufacturerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val viewHolder: ViewGroup = view.findViewById(R.id.manufacturer_details)
-        val imageView: ImageView = view.findViewById(R.id.manufacturer_logo)
-        val textView: TextView = view.findViewById(R.id.manufacturer_name)
+    class ManufacturerViewHolder(binding: ManufacturersListItemBinding) : RecyclerView.ViewHolder(binding
+        .root) {
+        val viewHolder = binding.manufacturerDetails
+        val imageView = binding.manufacturerLogo
+        val textView = binding.manufacturerName
     }
 
     /**
      * Creates new views with R.layout.manufacturers_list_item as its template.
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ManufacturerViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.manufacturers_list_item, parent, false)
+        val adapterLayout = ManufacturersListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ManufacturerViewHolder(adapterLayout)
     }
 
