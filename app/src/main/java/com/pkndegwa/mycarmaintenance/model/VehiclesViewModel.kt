bc30f695b8fb0a28e642480b96049fcf5e040d3b
Peter Kingori <kpndegwa@gmail.com>
2022-07-05 14:39:24 +0300

    Refactor VehiclesViewModel.kt file.

--- app/src/main/java/com/pkndegwa/mycarmaintenance/model/VehiclesViewModel.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/model/VehiclesViewModel.kt
@@ -7,24 +7,6 @@ import com.pkndegwa.mycarmaintenance.data.VehiclesData
  * The [ViewModel] that is attached to the VehiclesFragment.
  */
 class VehiclesViewModel : ViewModel() {
-    private var _vehicleType = ""
-    val vehicleType = _vehicleType
-
-    private var _manufacturer = ""
-    val manufacturer = _manufacturer
-
-    private var _model = ""
-    val model = _model
-
-    private var _licensePlate = ""
-    val licensePlate = _licensePlate
-
-    private var _fuelType = ""
-    val fuelType = _fuelType
-
-    private var _mileage = 0
-    val mileage = _mileage
-
     // The internal variable that stores a list of registered vehicles.
     private var _vehiclesData = ArrayList<Vehicle>()
 
