3c5d6702f9084f508c1e1f8f1eaff0e59ca24817
Peter Kingori <kpndegwa@gmail.com>
2022-08-15 17:37:12 +0300

    Mark the Vehicle.kt class as a Room Entity class.

--- app/src/main/java/com/pkndegwa/mycarmaintenance/model/Vehicle.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/model/Vehicle.kt
@@ -1,13 +1,26 @@
 package com.pkndegwa.mycarmaintenance.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
- * Data model for registered vehicles.
+ * Data model for registered vehicles that is also used as the database entity class for the vehicles table.
  */
+@Entity(tableName = "vehicles")
 data class Vehicle(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "type")
     val type: String,
+    @ColumnInfo(name = "manufacturer")
     val manufacturer: String,
+    @ColumnInfo(name = "model")
     val model: String,
+    @ColumnInfo(name = "license_plate")
     val licensePlate: String,
+    @ColumnInfo(name = "fuel_type")
     val fuelType: String,
-    val mileage: Int,
+    @ColumnInfo(name = "mileage")
+    val mileage: Int
 )
