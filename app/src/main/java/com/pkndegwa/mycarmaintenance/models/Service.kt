53a2a30cd0f36b369c5983262689112aec49792e
Peter Kingori <kpndegwa@gmail.com>
2022-11-01 14:22:21 +0300

    Add a Service model class that represents the maintenance service that is done to vehicles. Also add boilerplate code in AddServiceFragment.kt

--- app/src/main/java/com/pkndegwa/mycarmaintenance/models/Service.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/models/Service.kt
@@ -1,17 +1,30 @@
 package com.pkndegwa.mycarmaintenance.models
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Data model for service done that is also used as the database entity class for the services table.
  */
 @Entity(tableName = "services")
 data class Service(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "services_done_list")
     val servicesDoneList: MutableList<String>,
+    @ColumnInfo(name = "current_mileage")
     val currentMileage: Int,
+    @ColumnInfo(name = "next_service_mileage")
     val nextServiceMileage: Int,
+    @ColumnInfo(name = "total_cost")
     val totalCost: Double,
+    @ColumnInfo(name = "service_date")
     val serviceDate: String,
+    @ColumnInfo(name = "next_service_date")
     val nextServiceDate: String = "",
-    val notes: String = ""
+    @ColumnInfo(name = "notes")
+    val notes: String = "",
+    @ColumnInfo(name = "vehicle_id")
+    val vehicleId: Int
 )
--- app/src/main/java/com/pkndegwa/mycarmaintenance/ui/fragments/AddServiceFragment.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/ui/fragments/AddServiceFragment.kt
@@ -1,19 +1,39 @@
 package com.pkndegwa.mycarmaintenance.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
-import com.pkndegwa.mycarmaintenance.R
+import com.pkndegwa.mycarmaintenance.databinding.FragmentAddServiceBinding
 
 /**
  * [AddServiceFragment] allows a user to add a new service that has been performed to the vehicle.
  */
 class AddServiceFragment : Fragment() {
+    private var _binding: FragmentAddServiceBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_add_service, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Retrieve and inflate the layout for this fragment.
+        _binding = FragmentAddServiceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    /**
+     * Frees the binding object when the Fragment is destroyed.
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        // Hide keyboard.
+        val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as
+                InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+        _binding = null
     }
 }
