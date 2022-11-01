f809f72fd486c32df66df2062e09a53b784950e6
Peter Kingori <kpndegwa@gmail.com>
2022-11-01 18:15:14 +0300

    Add a type converter to the database because of the MutableList in the Service class being saved.

--- app/src/main/java/com/pkndegwa/mycarmaintenance/database/AppDatabase.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/database/AppDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.pkndegwa.mycarmaintenance.models.Service
 import com.pkndegwa.mycarmaintenance.models.Vehicle
+import com.pkndegwa.mycarmaintenance.utils.Converters
 
 @Database(entities = [Vehicle::class, Service::class], version = 3)
+@TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun vehicleDao(): VehicleDao
     abstract fun serviceDao(): ServiceDao
