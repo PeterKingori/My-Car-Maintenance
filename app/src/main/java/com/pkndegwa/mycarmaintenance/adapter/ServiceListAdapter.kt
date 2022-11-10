18bfce41f70417470772b90f66d5229d358cc351
Peter Kingori <kpndegwa@gmail.com>
2022-11-10 23:18:37 +0300

    Add formatting to number values.

--- app/src/main/java/com/pkndegwa/mycarmaintenance/adapter/ServiceListAdapter.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/adapter/ServiceListAdapter.kt
@@ -1,27 +1,32 @@
 package com.pkndegwa.mycarmaintenance.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.pkndegwa.mycarmaintenance.R
 import com.pkndegwa.mycarmaintenance.databinding.ServiceListItemBinding
 import com.pkndegwa.mycarmaintenance.models.Service
 
 class ServiceListAdapter : ListAdapter<Service, ServiceListAdapter.ServiceViewHolder>(DiffCallback) {
+    private lateinit var context: Context
 
     /**
      * Provides a reference for the views needed to display items in the list.
      */
     class ServiceViewHolder(private var binding: ServiceListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(service: Service) {
+        fun bind(service: Service, context: Context) {
             binding.apply {
                 servicesDoneList.text = service.servicesDoneList
                 serviceDateTextView.text = service.serviceDate
-                currentMileageTextView.text = service.currentMileage.toString()
-                nextServiceMileageTextView.text = service.nextServiceMileage.toString()
+                currentMileageTextView.text =
+                    context.getString(R.string.formatted_vehicle_mileage, service.currentMileage)
+                nextServiceMileageTextView.text =
+                    context.getString(R.string.formatted_vehicle_mileage, service.nextServiceMileage)
                 nextServiceDateTextView.text = service.nextServiceDate
-                totalCostTextView.text = service.totalCost.toString()
+                totalCostTextView.text = context.getString(R.string.formatted_total_cost, service.totalCost)
                 serviceNotesTextView.text = service.notes
 
                 servicesHeadingContainer.setOnClickListener {
@@ -39,6 +44,7 @@ class ServiceListAdapter : ListAdapter<Service, ServiceListAdapter.ServiceViewHo
      * Creates new views with R.layout.service_list_item as its template.
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServiceViewHolder {
+        context = parent.context
         val layoutInflater = ServiceListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ServiceViewHolder(layoutInflater)
     }
@@ -48,7 +54,7 @@ class ServiceListAdapter : ListAdapter<Service, ServiceListAdapter.ServiceViewHo
      */
     override fun onBindViewHolder(holder: ServiceViewHolder, position: Int) {
         val currentService = getItem(position)
-        holder.bind(currentService)
+        holder.bind(currentService, context)
     }
 
     companion object {
