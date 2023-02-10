8737526ede9a821c0bd1811b7a2b1fc5d75653cd
Peter Kingori <kpndegwa@gmail.com>
2023-02-10 11:03:12 +0300

    Show progress bar while loading webview.

--- app/src/main/java/com/pkndegwa/mycarmaintenance/ui/fragments/WebViewFragment.kt
+++ app/src/main/java/com/pkndegwa/mycarmaintenance/ui/fragments/WebViewFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.pkndegwa.mycarmaintenance.databinding.FragmentWebViewBinding
@@ -21,7 +24,7 @@ class WebViewFragment : Fragment() {
 
     private val navigationArgs: WebViewFragmentArgs by navArgs()
 
-
+    lateinit var progressBar: ProgressBar
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         _binding = FragmentWebViewBinding.inflate(inflater, container, false)
@@ -31,11 +34,18 @@ class WebViewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        progressBar = binding.progressBar
         val pageId = navigationArgs.page
+        val webView = binding.webView
+        webView.webViewClient = WebViewClient()
 
         if (pageId == 1) {
-            binding.webView.loadUrl(PRIVACY_POLICY)
+            try {
+                webView.loadUrl(PRIVACY_POLICY)
+            } catch (e: Exception) {
+                binding.textView.text = e.message
+            }
+
         }
 
         if (pageId == 2) {
@@ -50,5 +60,16 @@ class WebViewFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private inner class WebViewClient: android.webkit.WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            progressBar.visibility = View.GONE
+        }
     }
 }
